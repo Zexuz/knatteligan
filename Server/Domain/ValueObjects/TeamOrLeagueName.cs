418780f43362603a772527864db90b10f1d458ef
lindebergl <leon.lindeberg@hotmail.com>
2016-10-31 16:03:54 +0100

    Add inital League.cs and Team.cs logic. Also add respective repository logic.

---
+++ Server/Domain/ValueObjects/TeamOrLeagueName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace knatteligan.Domain.ValueObjects
+{
+    public class TeamOrLeagueName
+    {
+        public string Value { get; private set; }
+
+        public TeamOrLeagueName(string name)
+        {
+            if (IsLeagueName(name))
+            {
+                Value = name;
+            }
+            //TODO: Better exception.
+            else
+            {
+                throw new Exception("Bad name.");
+            }
+        }
+
+        //TODO: Better regex.
+        private static bool IsLeagueName(string name)
+        {
+            return Regex.IsMatch(name, @"[a-öA-Ö ]+");
+        }
+
+    }
+}
--- Server/Repositories/TeamRepository.cs
+++ Server/Repositories/TeamRepository.cs
@@ -1,7 +1,38 @@
-﻿namespace knatteligan.Repositories
+﻿using System.Collections.Generic;
+using knatteligan.Domain.Entities;
+
+namespace knatteligan.Repositories
 {
     public class TeamRepository
     {
+        private readonly List<Team> _teams = new List<Team>();
+
+        private static TeamRepository _instance;
+
+        public static TeamRepository GetInstance()
+        {
+            return _instance ?? (_instance = new TeamRepository());
+        }
+
+        public IEnumerable<Team> GetAllLeagues()
+        {
+            return _teams;
+        }
+
+        public void AddTeam(Team team)
+        {
+            _teams.Add(team);
+        }
+
+        public void RemoveTeam(Team team)
+        {
+            _teams.Remove(team);
+        }
+
+        public void AddPlayerToTeam(Player player)
+        {
+            
+        }
 
     }
 }

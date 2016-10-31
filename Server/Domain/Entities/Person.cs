19cb1b1a5b79f76ca1f6c9828d19af16c63ed355
Zexuz <Robin.Edbom@gmail.com>
2016-10-31 11:20:49 +0100

    Added some constructors and prop to Person.cs

--- Server/Domain/Entities/Person.cs
+++ Server/Domain/Entities/Person.cs
@@ -1,7 +1,16 @@
-﻿namespace knatteligan.Domain.Entities
+﻿using System;
+
+namespace knatteligan.Domain.Entities
 {
     public class Person
     {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
 
+        public Person(string name, DateTime dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
     }
 }

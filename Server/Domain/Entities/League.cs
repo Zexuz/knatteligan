d4adf2d5391acb98524af693d2e53c9d203c7ef4
lindebergl <leon.lindeberg@hotmail.com>
2016-10-31 12:03:44 +0100

    Test from sprint-1 branch

--- Server/Domain/Entities/League.cs
+++ Server/Domain/Entities/League.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace knatteligan.Domain.Entities
 {
     public class League
     {
         public string Name { get; set; }
+        public DateTime Dob { get; set; }
     }
 }

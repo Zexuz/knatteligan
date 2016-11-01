acad47e9f00de5828bbbf1f39807a91fea5bef1a
Zexuz <Robin.Edbom@gmail.com>
2016-11-01 09:56:44 +0100

    Refactored code

--- Testing/PersonalId.cs
+++ Testing/PersonalId.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using knatteligan.Domain.Entities;
-using knatteligan.Services;
 using NUnit.Framework;
 
 namespace Testing
@@ -26,7 +22,7 @@ namespace Testing
             var doa = new DateTime(1996, 11, 07);
 
             TestDelegate testDeligate = () => new knatteligan.Domain.ValueObjects.PersonalId(doa, "sdfgh");
-            Assert.That(testDeligate, Throws.TypeOf<knatteligan.Domain.ValueObjects.InvalidPersonalId>());
+            Assert.That(testDeligate, Throws.TypeOf<knatteligan.Domain.ValueObjects.InvalidPersonalIdException>());
         }
     }
 }

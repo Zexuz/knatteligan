4dd323350ec2d9b0c8729da1f3c68a8b4b7fe0b7
Daniel <arne@mail.com>
2016-11-01 14:07:59 +0100

    Lade till CreatePlayer, EditPlayer, CreateCoach, EditCoach

--- Server/Repositories/PersonRepository.cs
+++ Server/Repositories/PersonRepository.cs
@@ -1,3 +1,6 @@
+using knatteligan.Domain.Entities;
+using System;
+
 namespace knatteligan.Repositories
 {
     public class PersonRepository
@@ -16,5 +19,28 @@ namespace knatteligan.Repositories
             }
         }
 
+        internal void CreatePlayer(PersonName name, DateTime dob)
+        {
+            var player = new Player(name, dob);
+            people.Add(player);
+        }
+        internal void EditPlayer(Player player, PersonName name, DateTime dob)
+        {
+            player.Name = name;
+            player.DateOfBirth = dob;
+        }
+
+        internal void CreateCoach(PersonName name, PhoneNumber phoneNumber, Email email)
+        {
+            var coach = new Coach(name, phoneNumber, email);
+            people.Add(coach);
+        }
+
+        internal void EditCoach(Coach coach, PersonName name, PhoneNumber phoneNumber, Email email)
+        {
+            coach.Name = name;
+            coach.PhoneNumber = phoneNumber;
+            coach.Email = email;
+        }
     }
 }

2c91a06fbc661b1e78f685070b1c42ff2ab1a071
Daniel <arne@mail.com>
2016-11-01 15:12:15 +0100

    Skapade email value object

---
+++ Server/Domain/ValueObjects/Email.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace knatteligan.Domain.ValueObjects
+{
+    public class Email
+    {
+        public string Value { get; private set; }
+        public  Email(string email)
+        {
+            if (!isEmail(email))
+            {
+                throw new Exception("Bad Email");
+            }
+            Value = email;
+        }
+
+        private static bool isEmail(string email)
+        {
+            return Regex.IsMatch(email, @"^[A - Za - z0 - 9._ % +-] +@[A - Za - z0 - 9.-] +\.[A-Za-z]{2,6}$");
+        }
+    }
+}

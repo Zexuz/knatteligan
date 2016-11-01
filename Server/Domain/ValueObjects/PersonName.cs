97b2fbaa3293955a3f3999bd7b6074d803013263
James Johansson <shyco_2000@hotmail.com>
2016-11-01 13:47:00 +0100

    Added PersonName value object

---
+++ Server/Domain/ValueObjects/PersonName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace knatteligan.Domain.ValueObjects
+{
+    class PersonName 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public PersonName(string firstName, string lastName)
+        {
+            if (!IsName(firstName, lastName)) 
+                throw new Exception("Bad name.");
+            
+                FirstName = firstName;
+                LastName = lastName;
+            
+            
+        }
+
+        public PersonName()
+        {
+
+        }
+
+        public static bool IsName(string firstName, string lastName)
+        {
+            return Regex.IsMatch(firstName, "^[a - zA - ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'-]{2,30}$")
+                && Regex.IsMatch(lastName, "^[a - zA - ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'-]{2,30}$");
+        }
+        
+    }
+}

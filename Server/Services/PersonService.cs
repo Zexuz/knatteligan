4f874a66ffce2be839a3860aba690ca73c7e99ae
Daniel <arne@mail.com>
2016-11-01 14:07:59 +0100

    Lade till CreatePlayer, EditPlayer, CreateCoach, EditCoach

--- Server/Services/PersonService.cs
+++ Server/Services/PersonService.cs
@@ -11,13 +11,21 @@ namespace knatteligan.Services
             get { return PersonRepository.Instance; }
         }
 
-        public void CreatePlayer(Player player)
+        public void CreatePlayer(PersonName name, DateTime dob)
         {
-            repository.CreatePerson(player);
+            Repository.CreatePlayer(name, dob);
         }
-        public void EditPlayer(Player player)
+        public void EditPlayer(Player player, string name, DateTime dob)
         {
-            repository.EditPerson(player);
+            Repository.EditPlayer(player, name, dob);
+        }
+        public void CreateCoach(PersonName name, PhoneNumber phoneNumber, Email email)
+        {
+            Repository.CreateCoach(name, phoneNumber, email);
+        }
+        public void EditCoach(Coach coach, PersonName name, PhoneNumber phoneNumber, Email email)
+        {
+            Repository.EditCoach(coach, name, phoneNumber, email);
         }
 
     }

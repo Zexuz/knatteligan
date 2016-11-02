0c702811e8774441e7af57d63f31cccd36c9cc2b
Daniel <Daniel@greger.se>
2016-11-02 12:23:44 +0100

    La till tre props på match-entiteten. Guid, HomeTeam och AwayTeam

--- Server/Domain/Entities/Match.cs
+++ Server/Domain/Entities/Match.cs
@@ -1,7 +1,18 @@
-﻿namespace knatteligan.Domain.Entities
+﻿using System;
+
+namespace knatteligan.Domain.Entities
 {
     public class Match
     {
-
+        public Guid Guid { get; set; }
+        public Team HomeTeam { get; set; }
+        public Team AwayTeam { get; set; }
+        
+        public Match(Team homeTeam, Team awayTeam)
+        {
+            HomeTeam = homeTeam;
+            AwayTeam = awayTeam;
+            Guid = new Guid();
+        }
     }
 }

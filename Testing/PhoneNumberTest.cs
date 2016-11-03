615553051149543a9507821730fddfa0073f117e
lindebergl <leon.lindeberg@hotmail.com>
2016-11-03 11:43:20 +0100

    Repo stuff

---
+++ Testing/PhoneNumberTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using knatteligan.Domain.ValueObjects;
+
+namespace Testing
+{
+    [TestFixture]
+    public class PhoneNumberTest
+    {
+        [Test]
+        public void CheckValidPhoneNumber()
+        {
+
+            var phoneNumber = new PhoneNumber("0733209064");
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}

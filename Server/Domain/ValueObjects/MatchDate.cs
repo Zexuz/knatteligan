46faf6b59e26194f625e9b13b72b2335e51c09fc
Mikael Klaening <m.klaening@gmail.com>
2016-11-03 12:05:21 +0100

    Created Value Object for Match Date

---
+++ Server/Domain/ValueObjects/MatchDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knatteligan.Domain.ValueObjects
+{
+    class MatchDate
+    {
+
+        public DateTime Value  { get; set; }
+        public MatchDate(DateTime matchDate, DateTime leagueStartTime, DateTime endOfLeague)
+        {
+
+            if (!IsValid(matchDate, leagueStartTime, endOfLeague))
+            {
+                throw new Exception("Match date is not valid");
+            }
+            Value = matchDate;
+
+
+        }
+
+        private bool IsValid(DateTime matchDate, DateTime leagueStartTime, DateTime endOfLeague)
+        {           
+            return matchDate > leagueStartTime && matchDate < endOfLeague;
+        }
+    }
+}

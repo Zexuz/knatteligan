a60d94bf9a2319687de34e9516434857c9cbecf3
Zexuz <Robin.Edbom@gmail.com>
2016-11-03 14:06:02 +0100

    Added admin-wpf

---
+++ KnatteliganWPF/CreateLeague.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KnatteliganWPF
+{
+    /// <summary>
+    /// Interaction logic for CreateLeague.xaml
+    /// </summary>
+    public partial class CreateLeague : Window
+    {
+        public CreateLeague()
+        {
+            InitializeComponent();
+        }
+        private void AddTeam_Clicked(object sender, RoutedEventArgs e)
+        {
+            var addTeam = new AddTeam();
+            var addTeamResult = addTeam.ShowDialog();
+           
+        }
+        private void CloseCommandHandler_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

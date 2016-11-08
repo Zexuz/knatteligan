3ad0e7f5917f94011a059cf41af6c14678531cd3
Frransis <mravand@me.com>
2016-11-08 12:09:59 +0100

    Removed old props for MatchEvents and consolidated them into one prop List<MatchEvent>
    Added prop int Suspension

--- Server/Domain/Entities/Player.cs
+++ Server/Domain/Entities/Player.cs
@@ -5,23 +5,15 @@ namespace knatteligan.Domain.Entities
 {
     public class Player : TeamPerson
     {
-        public List<YellowCard> YellowCards { get; set; }
-        public List<RedCard> RedCards { get; set; }
-        public List<Assist> Assists { get; set; }
-        public List<Goal> Goals { get; set; }
+        public List<MatchEvent> MatchEvents { get; set; }
+        public int Suspension { get; set; }
         
 
-        //public Player(PersonName name, PersonalNumber personalNumber)
-        //{
-
-        //}
 
         public Player(PersonName name, PersonalNumber personalNumber, Team team) : base(name, personalNumber, team)
         {
-            Assists = new List<Assist>();
-            RedCards = new List<RedCard>();
-            YellowCards = new List<YellowCard>();
-            Goals = new List<Goal>();
+            MatchEvents = new List<MatchEvent>();
+           
         }
     }
 }

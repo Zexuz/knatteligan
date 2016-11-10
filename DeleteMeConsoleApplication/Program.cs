31b6b369955e7a16bf0e1c4f55a8a5ea60229c83
Zexuz <Robin.edbom@gmail.com>
2016-11-10 19:38:22 +0100

    Nothing to see here, move along!

--- DeleteMeConsoleApplication/Program.cs
+++ DeleteMeConsoleApplication/Program.cs
@@ -2,66 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+
 using knatteligan.Domain.Entities;
 using knatteligan.Domain.ValueObjects;
 using knatteligan.Helpers;
 using knatteligan.Repositories;
 
 
-namespace DeleteMeConsoleApplication
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            new TestClass().Start();
+namespace DeleteMeConsoleApplication {
 
-            Environment.Exit(0);
+    internal class Program {
 
-            /*
+        public static void Main(string[] args) {
+            // Our new "test" for createSeries
+            // new TestClass().Start();
+            // Environment.Exit(0);
 
-    var teamNames = new[] {"Mad Amigos", "Good Saints", "Crazy Mehicans", "Cool Cats", "The wolfs", "Cowboys"};
-              var teams = new List<Team>();
 
-              foreach (var teamName in teamNames)
-              {
-                  var team = new Team(new TeamName(teamName));
+            var teamNames = new[] {"Mad Amigos", "Good Saints", "Crazy Mehicans", "Cool Cats", "The wolfs", "Cowboys"};
+            var teams = new List<Team>();
 
-                  for (int i = 0; i < 15; i++)
-                  {
-                      var player = GenareNewPlayer(team);
-                      team.TeamPersons.Add(player.Id);
+            foreach (var teamName in teamNames) {
+                var team = new Team(new TeamName(teamName));
 
-                      PersonRepository.GetInstance().Add(player);
-                  }
+                for (int i = 0; i < 15; i++) {
+                    var player = GenareNewPlayer(team);
+                    team.TeamPersons.Add(player.Id);
 
-                  teams.Add(team);
-                  TeamRepository.GetInstance().Add(team);
-              }
+                    PersonRepository.GetInstance().Add(player);
+                }
 
-              Console.WriteLine(teams);
+                teams.Add(team);
+                TeamRepository.GetInstance().Add(team);
+            }
 
-  */
+            Console.WriteLine(teams);
         }
 
-        private static Player GenareNewPlayer(Team team)
-        {
+        private static Player GenareNewPlayer(Team team) {
             var randomName = GenerateNewPersonFirstAndLastName();
             var randomPersonalNumber = GenerateNewPersonalNumber();
 
             return new Player(randomName, randomPersonalNumber, team);
         }
 
-        private static PersonalNumber GenerateNewPersonalNumber()
-        {
+        private static PersonalNumber GenerateNewPersonalNumber() {
             var date = new DateTime(GenNewNumber(1990, 2010), GenNewNumber(1, 12), GenNewNumber(1, 28));
 
             var lastfour = $"{GenNewNumber(0, 10)}{GenNewNumber(0, 10)}{GenNewNumber(0, 10)}{GenNewNumber(0, 10)}";
             return new PersonalNumber(date, lastfour);
         }
 
-        private static PersonName GenerateNewPersonFirstAndLastName()
-        {
+        private static PersonName GenerateNewPersonFirstAndLastName() {
             var firstNames = new[] {"Jhon", "Adam", "Kevein", "Daniel", "Alex", "James", "Peter", "Sandra", "Roberto"};
             var lastNames = new[] {"Doe", "Smith", "Jhonsson", "Karlsson", "Kevinsson", "Moth", "Rodrigas", "Aleandro"};
 
@@ -70,36 +62,30 @@ namespace DeleteMeConsoleApplication
             return new PersonName(firstName, lastName);
         }
 
-        private static int GenNewNumber(int min, int max)
-        {
+        private static int GenNewNumber(int min, int max) {
             return new Random().Next(min, max);
         }
+
     }
 
-    internal class TestClass
-    {
+    internal class TestClass {
+
         private List<Team> _listOfTeams;
 
         private Dictionary<int, MatchWeek> _matches;
 
-        public void Start()
-        {
-            _listOfTeams = new List<Team>
-            {
-                new Team
-                {
+        public void Start() {
+            _listOfTeams = new List<Team> {
+                new Team {
                     Name = new TeamName("One")
                 },
-                new Team
-                {
+                new Team {
                     Name = new TeamName("Two")
                 },
-                new Team
-                {
+                new Team {
                     Name = new TeamName("Three")
                 },
-                new Team
-                {
+                new Team {
                     Name = new TeamName("Four")
                 }
             };
@@ -113,18 +99,15 @@ namespace DeleteMeConsoleApplication
             Console.WriteLine($"AllTeamsPlaysSameAmountOfMatchesAway: {AllTeamsPlaysSameAmountOfMatchesAway()}");
         }
 
-        public bool NrOfRoundsIsCorrect()
-        {
+        public bool NrOfRoundsIsCorrect() {
             var number = (_listOfTeams.Count - 1) * 2;
             return number == _matches.Count;
         }
 
-        public bool AllTeamsPlaysSameAmountOfMatchesHome()
-        {
+        public bool AllTeamsPlaysSameAmountOfMatchesHome() {
             var allMatches = new List<Match>();
 
-            foreach (var keys in _matches.Keys)
-            {
+            foreach (var keys in _matches.Keys) {
                 allMatches.AddRange(_matches[keys].Matches.Select(matchId => MatchRepository.GetInstance().Find(matchId)));
             }
 
@@ -133,12 +116,10 @@ namespace DeleteMeConsoleApplication
             return (4 == groupedMatches.ToList().Count);
         }
 
-        public bool AllTeamsPlaysSameAmountOfMatchesAway()
-        {
+        public bool AllTeamsPlaysSameAmountOfMatchesAway() {
             var allMatches = new List<Match>();
 
-            foreach (var keys in _matches.Keys)
-            {
+            foreach (var keys in _matches.Keys) {
                 allMatches.AddRange(_matches[keys].Matches.Select(matchId => MatchRepository.GetInstance().Find(matchId)));
             }
 
@@ -146,5 +127,7 @@ namespace DeleteMeConsoleApplication
 
             return (4 == groupedMatches.ToList().Count);
         }
+
     }
+
 }

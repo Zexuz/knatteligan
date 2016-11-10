4815da8a51dbd67b64edd9454b62e0afa37ed94a
Frransis <mravand@me.com>
2016-11-10 11:09:36 +0100

    Fixed suspension

--- Server/Domain/Entities/League.cs
+++ Server/Domain/Entities/League.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using knatteligan.Domain.ValueObjects;
+using knatteligan.Helpers;
 
 namespace knatteligan.Domain.Entities
 {
@@ -8,7 +9,7 @@ namespace knatteligan.Domain.Entities
     {
         public LeagueName Name { get; set; }
         public List<Team> Teams { get; set; }
-        public List<Match> Matches { get; set; }
+        public Dictionary<int, MatchWeek> MatchWeeks { get; set; }
         public DateTime StartDate { get; set; }     
 
         public League(LeagueName name, List<Team> teams)

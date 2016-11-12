e5b469db24e4c4187d1766f27f77bf423973749c
Zexuz <Robin.edbom@gmail.com>
2016-11-12 22:12:38 +0100

    Added Home/AwayTeamSquad to a match Refectored some code

--- Server/Domain/Entities/Match.cs
+++ Server/Domain/Entities/Match.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using knatteligan.Helpers;
 using knatteligan.Repositories;
 
 namespace knatteligan.Domain.Entities
@@ -13,56 +12,61 @@ namespace knatteligan.Domain.Entities
         public DateTime MatchDate { get; set; }
         public List<Guid> MatchEvents { get; set; }
 
+        public List<Guid> HomeTeamSquad;
+        public List<Guid> AwayTeamSquad;
+
+
+        private League _league;
+        private int _currentMatchWeekNr;
 
         public Match(Guid homeTeam, Guid awayTeam)
         {
 
             HomeTeam = homeTeam;
             AwayTeam = awayTeam;
+
+            HomeTeamSquad = new List<Guid>();
+            AwayTeamSquad = new List<Guid>();
+
+            SetLeagueAndCurrentMatchWeekNr();
         }
 
-        public void SuspendPlayer(Player player, int rounds)
-        {
-            League league = null;
-            var currentMatchWeek = 0;
-
+        private void SetLeagueAndCurrentMatchWeekNr() {
             var allLeages = LeagueRepository.GetInstance().GetAll().ToList();
             for (int i = 0; i < allLeages.Count(); i++)
             {
                 var currentLeage = allLeages[i];
 
-                for (int gameWeekIndex = 1; gameWeekIndex < currentLeage.MatchWeeks.Count; gameWeekIndex++)
-                {
+                for (int gameWeekIndex = 1; gameWeekIndex < currentLeage.MatchWeeks.Count; gameWeekIndex++) {
                     var gameWeekMatches = currentLeage.MatchWeeks[gameWeekIndex];
 
-                    for (int k = 0; k < gameWeekMatches.Matches.Count; k++)
-                    {
-                        var matchGuid = gameWeekMatches.Matches[k];
+                    if (gameWeekMatches.Matches.All(matchGuid => matchGuid != Id)) continue;
 
-                        if (matchGuid == Id)
-                        {
-                            league = currentLeage;
-                            currentMatchWeek = gameWeekIndex;
-                            break;
-                        }
-
-                    }
-
+                    _league = currentLeage;
+                    _currentMatchWeekNr = gameWeekIndex;
                 }
 
             }
 
-            if (league == null)
+            if (_league == null)
                 throw new Exception("Our leage is not in the database");
+        }
 
-            if (league.MatchWeeks.Count - currentMatchWeek + rounds < 0)
+        public bool IsPlayersuspended(Guid playerId) {
+            return _league.MatchWeeks[_currentMatchWeekNr].SuspendedPlayers.Contains(playerId);
+        }
+
+        public void SuspendPlayer(Player player, int rounds)
+        {
+
+            if (_league.MatchWeeks.Count - _currentMatchWeekNr+ rounds < 0)
             {
-                rounds = league.MatchWeeks.Count - currentMatchWeek;
+                rounds = _league.MatchWeeks.Count - _currentMatchWeekNr;
             }
 
-            for (int i = currentMatchWeek + 1; i < rounds + currentMatchWeek; i++)
+            for (int i = _currentMatchWeekNr+ 1; i < rounds + _currentMatchWeekNr; i++)
             {
-                league.MatchWeeks[i].SuspendedPlayers.Add(player.Id);
+                _league.MatchWeeks[i].SuspendedPlayers.Add(player.Id);
             }
 
 

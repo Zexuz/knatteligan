6b996b175f405e1f8288695e0291d413492ea0e2
Zexuz <Robin.edbom@gmail.com>
2016-11-13 15:00:08 +0100

    Added a startSquad window

---
+++ KnatteliganWPF/SetTeamSquadWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using knatteligan.Domain.Entities;
+
+namespace KnatteliganWPF {
+
+    public partial class SetTeamSquadWindow{
+
+        public List<Player> StartSquadPlayers { get; set; }
+
+        private readonly List<Player> _players;
+
+        public SetTeamSquadWindow(List<Player> players) {
+            InitializeComponent();
+
+            _players = players;
+
+            DataContext = this;
+        }
+
+
+        private void WindowActivated(object sender, EventArgs e) {
+            Resources["Players"] = _players;
+        }
+
+    }
+
+}

f763506a72fc2d285534a0922818d681743460b8
Zexuz <Robin.Edbom@gmail.com>
2016-11-14 15:11:56 +0100

    Fixed stuff /Scharam

--- Server/Services/MatchWeekService.cs
+++ Server/Services/MatchWeekService.cs
@@ -5,33 +5,25 @@ using knatteligan.Helpers;
 
 namespace knatteligan.Services
 {
-    public class MatchService
+    public class MatchWeekService
     {
-        private readonly League _league;
-        private readonly int _currentMatchWeek;
-        private readonly Match _match;
-
-        private readonly MatchRepositoryService _matchRepositoryService;
-
-
-        public MatchService(Guid matchId)
+        public MatchWeekService()
         {
-
-            _matchRepositoryService = new MatchRepositoryService();
-
-            _match = _matchRepositoryService.Find(matchId);
-            _league = MatchHelper.GetLeageFromMatchId(matchId);
-            _currentMatchWeek = MatchHelper.GetCurrentMatchWeekNr(_league, matchId);
         }
 
-        public bool IsPlayerSuspended(Guid playerId)
+        public bool IsPlayerSuspended(Guid playerId, Guid matchId)
         {
+            League _league;
+            int _currentMatchWeek;
+            SetVariables(out _league, out _currentMatchWeek, matchId);
             return _league.MatchWeeks[_currentMatchWeek].SuspendedPlayers.Contains(playerId);
         }
 
         public void SuspendPlayer(Player player, int rounds, Guid matchId)
         {
-
+            League _league;
+            int _currentMatchWeek;
+            SetVariables(out _league, out _currentMatchWeek, matchId);
             if (_league.MatchWeeks.Count - _currentMatchWeek + rounds < 0)
             {
                 rounds = _league.MatchWeeks.Count - _currentMatchWeek;
@@ -43,20 +35,25 @@ namespace knatteligan.Services
             }
         }
 
-        public void SetSuspensionLength(List<YellowCard> yellowCards, List<RedCard> redCards, Player player)
+        public void SetSuspensionLength(List<YellowCard> yellowCards, List<RedCard> redCards, Player player,
+            Guid matchId)
         {
             if (yellowCards.Count == 2)
             {
-                SuspendPlayer(player, 1, _match.Id);
+                SuspendPlayer(player, 1, matchId);
                 return;
             }
 
             if (redCards.Count == 1)
             {
-                SuspendPlayer(player, 3, _match.Id);
+                SuspendPlayer(player, 3, matchId);
             }
         }
 
+        private static void SetVariables(out League league, out int CurrentMatchWeekNr, Guid matchId)
+        {
+            league = MatchHelper.GetLeageFromMatchId(matchId);
+            CurrentMatchWeekNr = MatchHelper.GetCurrentMatchWeekNr(league, matchId);
+        }
     }
-
 }

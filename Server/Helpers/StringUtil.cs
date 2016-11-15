e90338bce7dd4dd803e536c946948c87e34a4b9b
Philip Ryström <prys45@hotmail.com>
2016-11-15 14:38:32 +0100

    Statsapp - Fixat sökfunktion/ruta - Lagt till Searchresultitem i value objects - lags till Searchservice i services - lagt till StringUtil i Helpers

---
+++ Server/Helpers/StringUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knatteligan.Helpers
+{
+    public static class StringUtil
+    {
+        public static bool Contains(this string str, string str2, bool ignoreCase)
+        {
+            str = (ignoreCase) ? str.ToLower() : str;
+            str2 = (ignoreCase) ? str2.ToLower() : str2;
+            return str.Contains(str2);
+        }
+    }
+}

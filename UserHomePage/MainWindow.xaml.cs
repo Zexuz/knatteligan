fbfeee750675d83b017a6c5f7e0ffa0a72408706
Daniel <Arne@greger.se>
2016-11-24 12:18:18 +0100

    Fixat opch trixat

--- UserHomePage/MainWindow.xaml.cs
+++ UserHomePage/MainWindow.xaml.cs
@@ -32,6 +32,10 @@ namespace UserHomePage
 
         private void ManageLeague_Clicked(object sender, MouseButtonEventArgs e)
         {
+            var leage = (knatteligan.Domain.Entities.League)LeagueList.SelectedItem;
+            if (leage == null) return;
+            League leagueWindow = new League(leage);
+            var addLeagueResult = leagueWindow.ShowDialog();
         }
 
         private void searchTextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -39,37 +43,6 @@ namespace UserHomePage
             var searchText = searchTextBox.Text;
             var foundMatch = searchService.Search(searchText, true);
             SearchList.ItemsSource = foundMatch;
-        }
-
-        private void GoToLeague_Click(object sender, RoutedEventArgs e)
-        {
-            var leage = (knatteligan.Domain.Entities.League) LeagueList.SelectedItem;
-            if (leage == null) return;
-            League leagueWindow = new League(leage);
-            var addLeagueResult = leagueWindow.ShowDialog();
-        }
-
-        private void GoToPlayerStats_Click(object sender, RoutedEventArgs e)
-        {
-            var leage = (knatteligan.Domain.Entities.League) LeagueList.SelectedItem;
-            if (leage == null) return;
-
-            var teams = leage.TeamIds.Select(_teamService.FindById).ToList();
-
-            PlayerStats playerStats = new PlayerStats(teams);
-            var playerStatsResult = playerStats.ShowDialog();
-        }
-
-        private void GoToMatchList_Click(object sender, RoutedEventArgs e)
-        {
-            MatchList matchList = new MatchList();
-            var matchListResult = matchList.ShowDialog();
-        }
-
-        private void GoToSerieSchedule_Click(object sender, RoutedEventArgs e)
-        {
-            SerieScheduleWindow serieScheduleWindow = new SerieScheduleWindow();
-            var serieScheduleResult = serieScheduleWindow.ShowDialog();
         }
     }
 }
